936a2eb2f5e13a2387ec55d215cf720bf1772155
yungxog <62614490+yungxog@users.noreply.github.com>
2020-10-08 20:18:55 +0200

    Add files via upload

---
+++ brantano-2.0-estore/frontend/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      
+        
+        <p>
+          app page
+        </p>
+        
+      
+    </div>
+  );
+}
+
+export default App;
