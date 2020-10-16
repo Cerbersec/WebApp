8c2538fd239084c2227c471b0b963afb601751ab
Cerbersec <58052983+Cerbersec@users.noreply.github.com>
2020-10-16 22:24:56 +0200

    added database connection code

--- brantano-2.0-estore/backend/server.js
+++ brantano-2.0-estore/backend/server.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const compression = require('compression')
+const createError = require('http-errors')
+const path = require('path')
 
 if(process.env.NODE_ENV !== 'production') {
     require('dotenv').config()
@@ -14,30 +16,47 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.static('../frontend/public'))
 
 // setup database connection
-//const connection_string = process.env.CONNECTION_STRING
+const { Sequelize } = require('sequelize')
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'mssql',
+    operatorsAliases: false,
+    pool: {
+        max: 10,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
+})
 
+// test database connection
+sequelize.authenticate().then(() => {
+    console.log('Connection has been established successfully')
+})
+.catch(error => {
+    console.log('Unable to connect to the database', error)
+})
 
 // routers
 const storeRouter = require('./routes/store.router')
 const userRouter = require('./routes/user.router')
 
-// main endpoint
-app.use('/home', express.static('../frontend/public'))
+// set static dir
+app.use(express.static(path.join(__dirname, 'public')))
 
-//store endpoint
+//endpoints
 app.use('/', storeRouter)
 app.use('/users', userRouter)
 
-//test
-app.get('/test', (req, res) => {
-    res.send('test route')
-})
 
 //page not found
-app.use((req, res) => {
-    res.status(404).json({
-        msg: 'Page could not be found on this server'
-    })
+app.use((req, res, next) => {
+    next(createError(404))
+})
+
+app.use((err, req, res, next) => {
+    res.status(err.status || 500).json(err);
 })
 
 app.listen(port, function() {
