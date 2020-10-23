57e9f54c76b37a1b43dbeab81dcb3dac884b972e
Cerbersec <58052983+Cerbersec@users.noreply.github.com>
2020-10-23 11:56:02 +0200

    added auth.js

--- brantano-2.0-estore/backend/controllers/user.controller.js
+++ brantano-2.0-estore/backend/controllers/user.controller.js
@@ -8,6 +8,8 @@ const ensureAuthenticated = require('../modules/ensureAuthenticated')
 const bcrypt = require('bcrypt')
 const TypedError = require('../modules/ErrorHandler')
 
+const { promisify } = require('util');
+
 //showcase method
 const getUsers = async(req, res, next) => {
     try {
@@ -91,10 +93,100 @@ const register = async(req, res, next) => {
 }
 
 
-const login = async(req, res, next) => {
-    //placeholder
+const login = async (req, res) => {
+    try {
+      const { email, password } = req.body;
+   
+      //TODO validate
+      if( !email || !password ) {
+        return res.status(400).render('login', {
+          message: 'Please provide an email and password'
+        })
+      }
+   
+      //verplaatsen naar userDb.js
+      //veranderen naar Sequelize
+      db.query('SELECT * FROM users WHERE email = ?', [email], async (error, results) => {
+        console.log(results);
+
+        if( !results || !(await bcrypt.compare(password, results[0].password)) ) {
+          res.status(401).render('login', {
+            message: 'Email or Password is incorrect'
+          })
+        } else {
+          const id = results[0].id;
+   
+          //veranderen naar config/jwt-config.js
+          const token = jwt.sign({ id }, process.env.JWT_SECRET, {
+            expiresIn: process.env.JWT_EXPIRES_IN
+          });
+   
+          console.log("The token is: " + token);
+   
+          const cookieOptions = {
+            expires: new Date(
+              Date.now() + process.env.JWT_COOKIE_EXPIRES * 24 * 60 * 60 * 1000
+            ),
+            httpOnly: true
+          }
+   
+          res.cookie('jwt', token, cookieOptions );
+          res.status(200).redirect("/");
+        }
+   
+      })
+   
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const isLoggedIn = async (req, res, next) => {
+    // console.log(req.cookies);
+    if( req.cookies.jwt) {
+      try {
+        //1) verify the token
+        const decoded = await promisify(jwt.verify)(req.cookies.jwt,
+        process.env.JWT_SECRET
+        );
+   
+        console.log(decoded);
+   
+        //2) Check if the user still exists
+        //verplaatsen naar userDb.js
+        db.query('SELECT * FROM users WHERE id = ?', [decoded.id], (error, result) => {
+          console.log(result);
+   
+          if(!result) {
+            return next();
+          }
+   
+          req.user = result[0];
+          console.log("user is")
+          console.log(req.user);
+          return next();
+   
+        });
+      } catch (error) {
+        console.log(error);
+        return next();
+      }
+    } else {
+      next();
+    }
+  }
+   
+const logout = async (req, res) => {
+    res.cookie('jwt', 'logout', {
+      expires: new Date(Date.now() + 2*1000),
+      httpOnly: true
+    });
+   
+    res.status(200).redirect('/');
 }
 
 exports.getUsers = getUsers
 exports.register = register
 exports.login = login
+exports.logout = logout
+exports.isLoggedIn = isLoggedIn
