48407ca92ab97ce3c1f75addafadb3cdca5c0483
rambosser <72037186+rambosser@users.noreply.github.com>
2020-10-27 11:39:37 +0100

    form verwijderd
    functionaliteit staat nu in product.js

--- brantano-2.0-estore/frontend/src/Components/Product/Product.js
+++ brantano-2.0-estore/frontend/src/Components/Product/Product.js
@@ -2,15 +2,50 @@ import React, { Component } from "react";
 import "./Product.css";
 import Bruin from '../../img/bruin.jpg';
 import { ButtonDropdown, DropdownToggle, DropdownMenu, DropdownItem, } from 'reactstrap';
-import Form from './Form'; 
+
+import Bestelling from 'C:/Users/bamsr/Documents/GitHub/WebApp/brantano-2.0-estore/frontend/src/Components/Bestelling/Bestelling.js';
+import Button from '@material-ui/core/Button';
+import { Link as RouterLink } from 'react-router-dom';
+import Link from '@material-ui/core/Link'
 
 
+
+var maat = 30;
+    var kleur="bruin";
 
 class Product extends React.Component{
 
+  constructor(props) {
+    super(props)
+
+    this.state = {
+            Technology: '30',
+            Kleur: 'Bruin'
+
+    }
+    
+
+}
+
+
+Changetechnology = (e) => {
+    this.setState({
+            Technology: e.target.value
+    })
+    maat = e.target.value;
+}
+
+Changekleur = (e) => {
+    this.setState({
+            Kleur: e.target.value
+    })
+    kleur = e.target.value;  
+}
 
   render() {
+    
     return (
+     
     <div>
       <div className="imageLine">
           <h2>Mooie schoenen</h2>
@@ -18,8 +53,55 @@ class Product extends React.Component{
         
       </div>
       <div className="inline">
+                  <div>
+                  <form onSubmit={this.onsubmit}>
+                          <div >
+                                  <div >
+                                          Kies uw maat
+                  </div>
+                                  <select value={this.state.Technology} onChange={this.Changetechnology} >
+                                          <option>30</option>
+                                          <option>31</option>
+                                          <option>32</option>
+                                          <option>33</option>
+                                          <option>34</option>
+                                          <option>35</option>
+
+                                  </select>
+                          </div>
+                          <div >
+                                  <div >
+                                          Kies uw kleur
+                  </div>
+                                  <select value={this.state.Kleur} onChange={this.Changekleur} >
+                                          <option>Bruin</option>
+                                          <option>Zwart</option>
+                                          <option>Groen</option>
+                                          <option>Beige</option>
+
+
+                                  </select>
+                          </div>
+                          <br></br>
+                          <div >
+                                  Morgen in huis
+                  </div>
+                  <Button color="primary" component={RouterLink} to={{
+    pathname: "/bestelling"
+  }} >
+                    Naar bestelling
+                  </Button>
+
+                  </form>
+
+          </div>
+                
+        
+
+        
       
-      <Form></Form>  
+      
+
 
           </div>
       </div>
