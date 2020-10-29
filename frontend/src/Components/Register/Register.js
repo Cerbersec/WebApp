03124385f8b6019429f8ac84e7d466e9f9579718
rambosser <72037186+rambosser@users.noreply.github.com>
2020-10-29 16:33:11 +0100

    register
    register pagina kan username en password aannemen. doet er wel nog niets mee

--- frontend/src/Components/Register/Register.js
+++ frontend/src/Components/Register/Register.js
@@ -1,10 +1,18 @@
 import React, {Component} from "react";
 import Avatar from "@material-ui/core/Avatar";
 import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Auth from "../../Auth";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
 
 class Register extends Component{
-   
+  state = {
+    userName: "",
+    pass: "",
+    redirectToReferrer: false
+  };
     render(){
+      
       return(
 
         <div
@@ -40,6 +48,32 @@ class Register extends Component{
             {" "}
             REGISTER{" "}
           </div>
+          <TextField
+            value={this.state.userName}
+            placeholder="User name"
+            onChange={e => {
+              this.setState({ userName: e.target.value });
+            }}
+          />
+          <TextField
+            value={this.state.pass}
+            type="password"
+            placeholder="Password"
+            onChange={e => {
+              this.setState({ pass: e.target.value });
+            }}
+          />
+          <Button
+            style={{ marginTop: 20, width: 200 }}
+            variant="outlined"
+            color="primary"
+            onClick={() => {
+              // Simulate authentication call
+              alert("Username " + this.state.userName + "\n Password: " + this.state.pass);
+            }}
+          >
+            Register
+          </Button>
           </div>
           </div>
       )
