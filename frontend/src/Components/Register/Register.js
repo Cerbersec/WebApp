2c294442eb795fec385a84c548370533039c5faa
yungxog <62614490+yungxog@users.noreply.github.com>
2020-10-29 15:54:34 +0100

    Update Register.js

--- frontend/src/Components/Register/Register.js
+++ frontend/src/Components/Register/Register.js
@@ -1,16 +1,47 @@
 import React, {Component} from "react";
+import Avatar from "@material-ui/core/Avatar";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 
 class Register extends Component{
    
     render(){
       return(
 
-        <div className="register">
-       <h2>register pagina</h2>
-       
-        
-        
-       </div>
+        <div
+        style={{
+          height: "100%",
+          display: "flex",
+          justifyContent: "center",
+
+          alignItems: "center"
+        }}
+      >
+        <div
+          style={{
+            height: 300,
+            width: 200,
+            padding: 30,
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            flexDirection: "column"
+          }}
+        >
+          <Avatar style={{ marginBottom: 10 }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <div
+            style={{
+              marginBottom: 20,
+              fontSize: 24,
+              textAlign: "center"
+            }}
+          >
+            {" "}
+            REGISTER{" "}
+          </div>
+          </div>
+          </div>
       )
     }
   }
