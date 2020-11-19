7c318c8025335dfbac429ff4756c74d292e67721
Cerbersec <58052983+Cerbersec@users.noreply.github.com>
2020-11-19 20:49:17 +0100

    fixed config using .env

--- Database/insert_into_everything_mysql.sql
+++ Database/insert_into_everything_mysql.sql
@@ -1,67 +1,67 @@
 	INSERT INTO categories(category_name, category_age, gender, created_at, updated_at)
-	VALUES ('Men', 'ADULT', 'M', '2020-10-15', '2020-10-15')
+	VALUES ('Men', 'ADULT', 'M', '2020-10-15', '2020-10-15');
 
 	INSERT INTO categories(category_name, category_age, gender, created_at, updated_at)
-	VALUES ('Women', 'ADULT', 'F', '2020-10-15', '2020-10-15')
+	VALUES ('Women', 'ADULT', 'F', '2020-10-15', '2020-10-15');
 
 	INSERT INTO categories(category_name, category_age, gender, created_at, updated_at)
-	VALUES ('Boys', 'CHILDREN', 'M', '2020-10-15', '2020-10-15')
+	VALUES ('Boys', 'CHILDREN', 'M', '2020-10-15', '2020-10-15');
 
 	INSERT INTO categories(category_name, category_age, gender, created_at, updated_at)
-	VALUES ('Girls', 'CHILDREN', 'F', '2020-10-15', '2020-10-15')
+	VALUES ('Girls', 'CHILDREN', 'F', '2020-10-15', '2020-10-15');
 	
 	
 
 	INSERT INTO customers(first_name, last_name, gender, phone, email_address, username, password, created_at, updated_at)
-	VALUES ('sander', 'forrer', 'M', '', 'sanderforrer@gmail.com', 'sanderf', 'test', '2020-10-15', '2020-10-15')
+	VALUES ('sander', 'forrer', 'M', '', 'sanderforrer@gmail.com', 'sanderf', 'test', '2020-10-15', '2020-10-15');
 
 	INSERT INTO addresses(street_name, postal_code, street_nr, bus_nr, city, country, created_at, updated_at, customer_id)
-	VALUES ('violetlaan', '2980', '47', '', 'zoersel', 'belgie', '2020-10-15', '2020-10-15', '1')
+	VALUES ('violetlaan', '2980', '47', '', 'zoersel', 'belgie', '2020-10-15', '2020-10-15', '1');
 	
 	
 	
 	
 	INSERT INTO products(brand,retail_price,name,release_date,price,stock_quantity,size,created_at, updated_at,color, popular, description, image_url, category_id)
-	VALUES ('Nike', '120', 'Nike Zoom Freak 2', '2020-10-15', '147', '4', '41','2020-10-15','2020-10-15','black', '1', 'Beautiful pair of shoes', 'https://image.shutterstock.com/image-photo/genuine-oil-pull-leather-men-600w-1105607897.jpg', '1')
+	VALUES ('Nike', '120', 'Nike Zoom Freak 2', '2020-10-15', '147', '4', '41','2020-10-15','2020-10-15','black', '1', 'Beautiful pair of shoes', 'https://image.shutterstock.com/image-photo/genuine-oil-pull-leather-men-600w-1105607897.jpg', '1');
 
 	INSERT INTO products(brand,retail_price,name,release_date,price,stock_quantity,size,created_at, updated_at,color, popular, description, image_url, category_id)
-	VALUES ('Jordan', '125', 'Jordan 1 Mid', '2020-10-15', '150', '10', '39','2020-10-15','2020-10-15','red', '1', 'Beautiful pair of shoes', 'https://image.shutterstock.com/image-photo/genuine-oil-pull-leather-men-600w-1105607897.jpg', '1')
+	VALUES ('Jordan', '125', 'Jordan 1 Mid', '2020-10-15', '150', '10', '39','2020-10-15','2020-10-15','red', '1', 'Beautiful pair of shoes', 'https://image.shutterstock.com/image-photo/genuine-oil-pull-leather-men-600w-1105607897.jpg', '1');
 
 	INSERT INTO products(brand,retail_price,name,release_date,price,stock_quantity,size,created_at, updated_at,color, popular, description, image_url, category_id)
-	VALUES ('Nike', '230', 'Nike Zoom Freak 3', '2020-10-15', '276', '20', '12','2020-10-15','2020-10-15','blue', '1', 'Beautiful pair of shoes', 'https://image.shutterstock.com/image-photo/genuine-oil-pull-leather-men-600w-1105607897.jpg', '2')
+	VALUES ('Nike', '230', 'Nike Zoom Freak 3', '2020-10-15', '276', '20', '12','2020-10-15','2020-10-15','blue', '1', 'Beautiful pair of shoes', 'https://image.shutterstock.com/image-photo/genuine-oil-pull-leather-men-600w-1105607897.jpg', '2');
 
 	INSERT INTO products(brand,retail_price,name,release_date,price,stock_quantity,size,created_at, updated_at,color, popular, description, image_url, category_id)
-	VALUES ('Nike', '120', 'Nike Zoom Freak 2', '2020-10-15', '147', '4', '41','2020-10-15','2020-10-15','black', '1', 'Beautiful pair of shoes', 'https://image.shutterstock.com/image-photo/genuine-oil-pull-leather-men-600w-1105607897.jpg', '2')
+	VALUES ('Nike', '120', 'Nike Zoom Freak 2', '2020-10-15', '147', '4', '41','2020-10-15','2020-10-15','black', '1', 'Beautiful pair of shoes', 'https://image.shutterstock.com/image-photo/genuine-oil-pull-leather-men-600w-1105607897.jpg', '2');
 
 	INSERT INTO products(brand,retail_price,name,release_date,price,stock_quantity,size,created_at, updated_at,color, popular, description, image_url, category_id)
-	VALUES ('Jordan', '125', 'Jordan 1 Mid', '2020-10-15', '150', '10', '39','2020-10-15','2020-10-15','red', '1', 'Beautiful pair of shoes', 'https://image.shutterstock.com/image-photo/genuine-oil-pull-leather-men-600w-1105607897.jpg', '3')
+	VALUES ('Jordan', '125', 'Jordan 1 Mid', '2020-10-15', '150', '10', '39','2020-10-15','2020-10-15','red', '1', 'Beautiful pair of shoes', 'https://image.shutterstock.com/image-photo/genuine-oil-pull-leather-men-600w-1105607897.jpg', '3');
 
 	INSERT INTO products(brand,retail_price,name,release_date,price,stock_quantity,size,created_at, updated_at,color, popular, description, image_url, category_id)
-	VALUES ('Nike', '230', 'Nike Zoom Freak 3', '2020-10-15', '276', '20', '12','2020-10-15','2020-10-15','blue', '1', 'Beautiful pair of shoes', 'https://image.shutterstock.com/image-photo/genuine-oil-pull-leather-men-600w-1105607897.jpg', '3')
+	VALUES ('Nike', '230', 'Nike Zoom Freak 3', '2020-10-15', '276', '20', '12','2020-10-15','2020-10-15','blue', '1', 'Beautiful pair of shoes', 'https://image.shutterstock.com/image-photo/genuine-oil-pull-leather-men-600w-1105607897.jpg', '3');
 
 	INSERT INTO products(brand,retail_price,name,release_date,price,stock_quantity,size,created_at, updated_at,color, popular, description, image_url, category_id)
-	VALUES ('Nike', '120', 'Nike Zoom Freak 2', '2020-10-15', '147', '4', '41','2020-10-15','2020-10-15','black', '1', 'Beautiful pair of shoes', 'https://image.shutterstock.com/image-photo/genuine-oil-pull-leather-men-600w-1105607897.jpg', '3')
+	VALUES ('Nike', '120', 'Nike Zoom Freak 2', '2020-10-15', '147', '4', '41','2020-10-15','2020-10-15','black', '1', 'Beautiful pair of shoes', 'https://image.shutterstock.com/image-photo/genuine-oil-pull-leather-men-600w-1105607897.jpg', '3');
 
 	INSERT INTO products(brand,retail_price,name,release_date,price,stock_quantity,size,created_at, updated_at,color, popular, description, image_url, category_id)
-	VALUES ('Jordan', '125', 'Jordan 1 Mid', '2020-10-15', '150', '10', '39','2020-10-15','2020-10-15','red', '1', 'Beautiful pair of shoes', 'https://image.shutterstock.com/image-photo/genuine-oil-pull-leather-men-600w-1105607897.jpg', '3')
+	VALUES ('Jordan', '125', 'Jordan 1 Mid', '2020-10-15', '150', '10', '39','2020-10-15','2020-10-15','red', '1', 'Beautiful pair of shoes', 'https://image.shutterstock.com/image-photo/genuine-oil-pull-leather-men-600w-1105607897.jpg', '3');
 
 	INSERT INTO products(brand,retail_price,name,release_date,price,stock_quantity,size,created_at, updated_at,color, popular, description, image_url, category_id)
-	VALUES ('Nike', '230', 'Nike Zoom Freak 3', '2020-10-15', '276', '20', '12','2020-10-15','2020-10-15','blue', '1', 'Beautiful pair of shoes', 'https://image.shutterstock.com/image-photo/genuine-oil-pull-leather-men-600w-1105607897.jpg', '4')
+	VALUES ('Nike', '230', 'Nike Zoom Freak 3', '2020-10-15', '276', '20', '12','2020-10-15','2020-10-15','blue', '1', 'Beautiful pair of shoes', 'https://image.shutterstock.com/image-photo/genuine-oil-pull-leather-men-600w-1105607897.jpg', '4');
 
 	INSERT INTO products(brand,retail_price,name,release_date,price,stock_quantity,size,created_at, updated_at,color, popular, description, image_url, category_id)
-	VALUES ('Nike', '120', 'Nike Zoom Freak 2', '2020-10-15', '147', '4', '41','2020-10-15','2020-10-15','black', '1', 'Beautiful pair of shoes', 'https://image.shutterstock.com/image-photo/genuine-oil-pull-leather-men-600w-1105607897.jpg', '4')
+	VALUES ('Nike', '120', 'Nike Zoom Freak 2', '2020-10-15', '147', '4', '41','2020-10-15','2020-10-15','black', '1', 'Beautiful pair of shoes', 'https://image.shutterstock.com/image-photo/genuine-oil-pull-leather-men-600w-1105607897.jpg', '4');
 
 	INSERT INTO products(brand,retail_price,name,release_date,price,stock_quantity,size,created_at, updated_at,color, popular, description, image_url, category_id)
-	VALUES ('Jordan', '125', 'Jordan 1 Mid', '2020-10-15', '150', '10', '39','2020-10-15','2020-10-15','red', '1', 'Beautiful pair of shoes', 'https://image.shutterstock.com/image-photo/genuine-oil-pull-leather-men-600w-1105607897.jpg', '4')
+	VALUES ('Jordan', '125', 'Jordan 1 Mid', '2020-10-15', '150', '10', '39','2020-10-15','2020-10-15','red', '1', 'Beautiful pair of shoes', 'https://image.shutterstock.com/image-photo/genuine-oil-pull-leather-men-600w-1105607897.jpg', '4');
 
 	INSERT INTO products(brand,retail_price,name,release_date,price,stock_quantity,size,created_at, updated_at,color, popular, description, image_url, category_id)
-	VALUES ('Nike', '230', 'Nike Zoom Freak 3', '2020-10-15', '276', '20', '12','2020-10-15','2020-10-15','blue', '1', 'Beautiful pair of shoes', 'https://image.shutterstock.com/image-photo/genuine-oil-pull-leather-men-600w-1105607897.jpg', '4')
+	VALUES ('Nike', '230', 'Nike Zoom Freak 3', '2020-10-15', '276', '20', '12','2020-10-15','2020-10-15','blue', '1', 'Beautiful pair of shoes', 'https://image.shutterstock.com/image-photo/genuine-oil-pull-leather-men-600w-1105607897.jpg', '4');
 
 	INSERT INTO products(brand,retail_price,name,release_date,price,stock_quantity,size,created_at, updated_at,color, popular, description, image_url, category_id)
-	VALUES ('Nike', '120', 'Nike Zoom Freak 2', '2020-10-15', '147', '4', '41','2020-10-15','2020-10-15','black', '1', 'Beautiful pair of shoes', 'https://image.shutterstock.com/image-photo/genuine-oil-pull-leather-men-600w-1105607897.jpg', '1')
+	VALUES ('Nike', '120', 'Nike Zoom Freak 2', '2020-10-15', '147', '4', '41','2020-10-15','2020-10-15','black', '1', 'Beautiful pair of shoes', 'https://image.shutterstock.com/image-photo/genuine-oil-pull-leather-men-600w-1105607897.jpg', '1');
 
 	INSERT INTO products(brand,retail_price,name,release_date,price,stock_quantity,size,created_at, updated_at,color, popular, description, image_url, category_id)
-	VALUES ('Jordan', '125', 'Jordan 1 Mid', '2020-10-15', '150', '10', '39','2020-10-15','2020-10-15','red', '1', 'Beautiful pair of shoes', 'https://image.shutterstock.com/image-photo/genuine-oil-pull-leather-men-600w-1105607897.jpg', '2')
+	VALUES ('Jordan', '125', 'Jordan 1 Mid', '2020-10-15', '150', '10', '39','2020-10-15','2020-10-15','red', '1', 'Beautiful pair of shoes', 'https://image.shutterstock.com/image-photo/genuine-oil-pull-leather-men-600w-1105607897.jpg', '2');
 
 	INSERT INTO products(brand,retail_price,name,release_date,price,stock_quantity,size,created_at, updated_at,color, popular, description, image_url, category_id)
-	VALUES ('Nike', '230', 'Nike Zoom Freak 3', '2020-10-15', '276', '20', '12','2020-10-15','2020-10-15','blue', '1', 'Beautiful pair of shoes', 'https://image.shutterstock.com/image-photo/genuine-oil-pull-leather-men-600w-1105607897.jpg', '2')
+	VALUES ('Nike', '230', 'Nike Zoom Freak 3', '2020-10-15', '276', '20', '12','2020-10-15','2020-10-15','blue', '1', 'Beautiful pair of shoes', 'https://image.shutterstock.com/image-photo/genuine-oil-pull-leather-men-600w-1105607897.jpg', '2');
--- brantayes/backend/config/database.js
+++ brantayes/backend/config/database.js
@@ -7,7 +7,7 @@ const { Sequelize } = require('sequelize')
 module.exports = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
-    dialect: 'mysql',
+    dialect: process.env.DB_DIALECT,
     dialectOptions: {
         encrypt: true
     },
