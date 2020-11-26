07ad657b9738012d1637bfa6140049326bef922c
JCPilaet <72446919+JCPilaet@users.noreply.github.com>
2020-11-26 20:31:09 +0100

    Update Account.js

--- brantayes/frontend/src/Components/Account/Account.js
+++ brantayes/frontend/src/Components/Account/Account.js
@@ -1,4 +1,9 @@
 import React, { Component } from "react";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
 
 class Account extends Component 
 {
@@ -7,7 +12,27 @@ class Account extends Component
         return (
             <div className="AccountPage">
                 <h1>Account Page</h1>
-                <h2>Order History</h2>
+                <div className="Order History">    
+                    <h2>Order History</h2>
+                    <Table>
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>Item Name</TableCell>
+                                <TableCell>Price</TableCell>
+                                <TableCell>Quantity</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            <TableRow>
+                                <TableCell>temp</TableCell>
+                                <TableCell>temp</TableCell>
+                                <TableCell>temp</TableCell>
+                            </TableRow>
+                        </TableBody>
+                    </Table>
+                </div>
+                
+                <h1>!Under Construction!</h1>
             </div>
         )
     }
