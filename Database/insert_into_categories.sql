359f1de03d0e847c8d7e33f04d9c0cf96cd90988
Cerbersec <58052983+Cerbersec@users.noreply.github.com>
2020-12-02 19:18:40 +0100

    updated insert scripts to use production data

---
+++ Database/insert_into_categories.sql
@@ -0,0 +1,14 @@
+INSERT INTO brantano.dbo.categories(category_name, category_age, gender, created_at, updated_at)
+VALUES ('Men', 'ADULT', 'M', '2020-10-15', '2020-10-15')
+
+INSERT INTO brantano.dbo.categories(category_name, category_age, gender, created_at, updated_at)
+VALUES ('Women', 'ADULT', 'F', '2020-10-15', '2020-10-15')
+
+INSERT INTO brantano.dbo.categories(category_name, category_age, gender, created_at, updated_at)
+VALUES ('Children', 'CHILDREN', 'X', '2020-10-15', '2020-10-15')
+
+INSERT INTO brantano.dbo.categories(category_name, category_age, gender, created_at, updated_at)
+VALUES ('Preschool', 'CHILDREN', 'X', '2020-10-15', '2020-10-15')
+
+INSERT INTO brantano.dbo.categories(category_name, category_age, gender, created_at, updated_at)
+VALUES ('Toddler', 'CHILDREN', 'X', '2020-10-15', '2020-10-15')
