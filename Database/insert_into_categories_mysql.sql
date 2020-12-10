4f30c62b93b0e6d02aa73af79cb3d579b545db3f
Cerbersec <cerbersec@gmail.com>
2020-12-10 14:00:41 +0000

    syntax fix

--- Database/insert_into_categories_mysql.sql
+++ Database/insert_into_categories_mysql.sql
@@ -1,14 +1,10 @@
 INSERT INTO categories(category_name, category_age, gender, created_at, updated_at)
-VALUES ('Men', 'ADULT', 'M', '2020-10-15', '2020-10-15')
-
+VALUES ('Men', 'ADULT', 'M', '2020-10-15', '2020-10-15');
 INSERT INTO categories(category_name, category_age, gender, created_at, updated_at)
-VALUES ('Women', 'ADULT', 'F', '2020-10-15', '2020-10-15')
-
+VALUES ('Women', 'ADULT', 'F', '2020-10-15', '2020-10-15');
 INSERT INTO categories(category_name, category_age, gender, created_at, updated_at)
-VALUES ('Children', 'CHILDREN', 'X', '2020-10-15', '2020-10-15')
-
+VALUES ('Children', 'CHILDREN', 'X', '2020-10-15', '2020-10-15');
 INSERT INTO categories(category_name, category_age, gender, created_at, updated_at)
-VALUES ('Preschool', 'CHILDREN', 'X', '2020-10-15', '2020-10-15')
-
+VALUES ('Preschool', 'CHILDREN', 'X', '2020-10-15', '2020-10-15');
 INSERT INTO categories(category_name, category_age, gender, created_at, updated_at)
-VALUES ('Toddler', 'CHILDREN', 'X', '2020-10-15', '2020-10-15')
+VALUES ('Toddler', 'CHILDREN', 'X', '2020-10-15', '2020-10-15');
