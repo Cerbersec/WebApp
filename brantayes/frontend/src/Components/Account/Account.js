49c1adfeb65458f1afb7752699b503443c0d1c99
Cerbersec <58052983+Cerbersec@users.noreply.github.com>
2020-12-21 04:07:05 +0100

    cleaned up account page information and restyled forms

--- brantayes/frontend/src/Components/Account/Account.js
+++ brantayes/frontend/src/Components/Account/Account.js
@@ -1,17 +1,16 @@
 import React, { Component } from "react";
-import Table from "@material-ui/core/Table";
-import TableBody from "@material-ui/core/TableBody";
-import TableCell from "@material-ui/core/TableCell";
-import TableHead from "@material-ui/core/TableHead";
-import TableRow from "@material-ui/core/TableRow";
-import CircularProgress from "@material-ui/core/CircularProgress";
-import Api from "../../Api";
 import { withRouter } from "react-router-dom";
 import { connect } from "react-redux";
-import Button from "@material-ui/core/Button";
 import "./Account.css";
+
+import CircularProgress from "@material-ui/core/CircularProgress";
+import Button from "@material-ui/core/Button";
 import Select from "@material-ui/core/Select";
 import MenuItem from '@material-ui/core/MenuItem';
+
+import { Table, TableBody, TableCell, TableHead, TableRow } from "@material-ui/core";
+
+import Api from "../../Api";
 
 const mapStateToProps = state => {
     const { user } = state;
@@ -31,8 +30,8 @@ class Account extends Component
         this.state = {
             orders: [],
             customer: null,
-            //address: null,
-            loading: false
+            loading: false,
+            fieldsetDisabled: true,
         };
     }
 
@@ -40,7 +39,6 @@ class Account extends Component
     componentDidMount() {
         this.fetchOrders();
         this.fetchCustomer();
-        //this.fetchCustomerAddress();
     }
 
     async fetchOrders() {
@@ -53,91 +51,73 @@ class Account extends Component
         this.setState({loading: true})
         let response = await Api.getCustomerByID()
         this.setState({customer: response.customer, loading: false})
-        console.log(response)
     }
-
-    // async fetchCustomerAddress() {
-    //     this.setState({loading: true})
-    //     let response = await Api.getAddressByCustomerID()
-    //     this.setState({address: response, loading: false})
-    // }  
 
     render()
     {
-        if (this.state.loading) {
-            return <CircularProgress className="circular" />;
-        }
-
-        if (!this.state.customer) {
+        if (this.state.loading || !this.state.customer) {
             return <CircularProgress className="circular" />;
         }
 
         return (
-
-            <div className="AccountPage">
+            <div id="accountpage">
                 <h1>My Brantayes account</h1>
-                <br></br>
-                <h2>Account information</h2>
-                <div class="row">
-                    <div class="col-md-6 personal-info">
-                        <h5>Personal info</h5>
-                        <form class="personal_info" action="">
-                            <label>Name: </label>
-                            <input type="text" id="name" placeholder={this.state.customer.first_name + " " + this.state.customer.last_name} ></input><br></br>
-                            <label>Username: </label>
-                            <input type="text" placeholder={this.state.customer.username}></input><br></br>
-                            {/* <label>Password: </label>
-                            <input type="password" value={this.state.customer.password}></input><br></br> */}
-                            {/* <label>Gender: </label>
-                            <input type="text" value={this.state.customer.gender}></input><br></br> */}
-                            <div class="row">
-                                <label>Gender: </label>
-                                <Select value={this.state.customer.gender}>
-                                    <MenuItem value={"M"}>Male</MenuItem>
-                                    <MenuItem value={"F"}>Female</MenuItem>
-                                    <MenuItem value={"X"}>Other</MenuItem>
-                                </Select>
-                            </div>
-                            <button type="button" class="btn btn-primary" onclick="unlockPersonalSection()">Edit</button>
+                <h2>Account information</h2>           
+                <div className="row border">                   
+                    <div className="col-md-6">
+                        <form>
+                            <fieldset disabled={this.state.fieldsetDisabled}>
+                                <div className="row">
+                                    <div className="col-md-6">
+                                        <input className="form-control" type="text" value={this.state.customer.first_name} />
+                                        <input className="form-control" type="text" value={this.state.customer.last_name} />
+                                        <input className="form-control" type="text" value={this.state.customer.phone} />
+                                    </div>
+                                    <div className="col-md-6">
+                                        <input className="form-control" type="text" value={this.state.customer.username} />
+                                        <Select className="form-control" disableUnderline disabled={this.state.fieldsetDisabled}>
+                                            <MenuItem value={"M"}>Male</MenuItem>
+                                            <MenuItem value={"F"}>Female</MenuItem>
+                                            <MenuItem value={"X"}>Other</MenuItem>
+                                        </Select>
+                                    </div>
+                                </div>
+                            </fieldset>
                         </form>
                     </div>
 
-                    <div class="col-md-6 contact-info">
-                        <h5>Contact info</h5>
-                        <form class="contact_info">
-                            <label>E-mail: </label>
-                            <input type="email" placeholder={this.state.customer.email_address} ></input><br></br>
-                            <label>Phone: </label>
-                            <input placeholder={this.state.customer.phone}></input><br></br>
-                            <label for="address">Address: </label>
-                            <input type="text" placeholder={this.state.customer.Addresses[0].street_name + " " + 
-                                this.state.customer.Addresses[0].street_nr + ", bus " +
-                                this.state.customer.Addresses[0].bus_nr}>
-                            </input><br></br>
-                            <label>City: </label>
-                            <input placeholder={this.state.customer.Addresses[0].postal_code + " " + 
-                                this.state.customer.Addresses[0].city + ", " +
-                                this.state.customer.Addresses[0].country}
-                            ></input><br></br>
-                            <button type="button" class="btn btn-primary" onclick="unlockConctactSection()">Edit</button>
+                    <div className="col-md-6">
+                        <form>
+                            <fieldset disabled={this.state.fieldsetDisabled}>
+                                <div className="row">
+                                    <div className="col-md-6">                               
+                                        <input className="form-control" type="text" value={this.state.customer.Addresses[0].street_name} />
+                                        <input className="form-control" type="text" value={this.state.customer.Addresses[0].postal_code} />
+                                        <input className="form-control" type="text" value={this.state.customer.Addresses[0].city} />
+                                    </div>
+                                    <div className="col-md-6">
+                                        <input className="form-control" type="text" value={this.state.customer.Addresses[0].street_nr} />
+                                        <input className="form-control" type="text" value={this.state.customer.Addresses[0].bus_nr} />
+                                        <input className="form-control" type="text" value={this.state.customer.Addresses[0].country} />
+                                    </div> 
+                                </div>
+                            </fieldset>
                         </form>
                     </div>
                 </div>
+                <div className="row">
+                    <Button
+                        className="form-btn"
+                        variant="outlined"
+                        color="primary"
+                        onClick={() => this.setState({ fieldsetDisabled: !this.state.fieldsetDisabled})} //TODO: handle form submit
+                    >
+                        {this.state.fieldsetDisabled ? ("Edit") : ("Save")}
+                    </Button>
+                </div>
 
-                {/* <script>
-                function unlockPersonalSection() {
-                    // var soap = document.getElementsByClassName("personal_info");
-                    // soap.attr({'disabled': 'enabled'});
-                    window.alert("does this even work?")
-                }
-                </script> */}
-                                
-                <br></br>
-                <br></br>
-                <br></br>
-
-                <div className="Order History"> 
-                    <h2>Order history</h2>
+                <h2>Order history</h2>
+                <div className="row">                   
                     <Table>
                         <TableHead>
                             <TableRow>
@@ -154,10 +134,27 @@ class Account extends Component
                                 <TableCell> {order.order_id} </TableCell>
                                 <TableCell> &euro; {order.total_price} </TableCell>
                                 <TableCell> &euro; {order.shipping_costs} </TableCell>
-                                <TableCell> {new Date(order.order_date).toLocaleString('nl-BE')/*.toISOString().split('T')[0]*/} </TableCell>
-                                <TableCell> {order.paid ? <Button variant="outlined" color="primary" style={{width:"160px"}} 
-                                onClick={() => {this.props.history.push("/orderdetails/" + order.order_id);}}> View details</Button>
-                                            : <Button variant="outlined" color="primary" style={{width:"160px"}}> Checkout </Button> } </TableCell>
+                                <TableCell> {new Date(order.order_date).toLocaleString('nl-BE')} </TableCell>
+                                <TableCell>
+                                    {order.paid ? (
+                                        <Button 
+                                            variant="outlined"
+                                            color="primary"
+                                            style={{width:"160px"}} 
+                                            onClick={() => {this.props.history.push("/orderdetails/" + order.order_id);}}
+                                        >
+                                            View details
+                                        </Button>
+                                    ) : ( 
+                                        <Button
+                                            variant="outlined"
+                                            color="primary"
+                                            style={{width:"160px"}}
+                                        >
+                                            Checkout
+                                        </Button>
+                                    )}
+                                </TableCell>
                             </TableRow>
                         })}
                         </TableBody>
