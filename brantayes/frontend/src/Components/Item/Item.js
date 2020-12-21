e33c19e1290d456d2b756dcc0893216999afd5d8
Cerbersec <58052983+Cerbersec@users.noreply.github.com>
2020-12-21 19:40:51 +0100

    cleaned up unused imports and removed comments

--- brantayes/frontend/src/Components/Item/Item.js
+++ brantayes/frontend/src/Components/Item/Item.js
@@ -3,14 +3,6 @@ import { withRouter } from "react-router-dom";
 import { connect } from "react-redux";
 import { addItemInCart } from "../../Redux/Actions";
 import AddShoppingCartIcon from "@material-ui/icons/AddShoppingCart";
-
-import IconButton from "@material-ui/core/IconButton";
-import Card from "@material-ui/core/Card";
-import CardActionArea from "@material-ui/core/CardActionArea";
-import CardActions from "@material-ui/core/CardActions";
-import CardContent from "@material-ui/core/CardContent";
-import CardMedia from "@material-ui/core/CardMedia";
-import Tooltip from "@material-ui/core/Tooltip";
 import Button from "@material-ui/core/Button";
 
 import "./Item.css";
@@ -79,60 +71,6 @@ class ConnectedItem extends Component {
                 </div>
             </div>
         </div>
-
-    /*
-      <Card
-        style={{ width: 200, height: 270, margin: 10, display: "inline-block" }}
-      >
-        <CardActionArea
-          onClick={() => {
-            this.props.history.push("/details/" + this.props.item.product_id);//id
-          }}
-        >
-          <CardMedia
-            style={{ height: 140 }}
-            image={this.props.item.image_url}
-          />
-          <CardContent style={{ height: 50 }}>
-            <div
-              style={{
-                marginLeft: 5,
-                fontWeight: "bold",
-                whiteSpace: "nowrap",
-                overflow: "hidden",
-                textOverflow: "ellipsis"
-              }}
-            >
-              {this.props.item.name}
-            </div>
-            <div style={{ margin: 5 }}>Price: &euro; {this.props.item.retail_price} </div>
-            <div style={{ color: "#1a9349", fontWeight: "bold", margin: 5 }}>
-              {this.props.item.popular > 3 && "Popular"}
-            </div>
-          </CardContent>
-        </CardActionArea>
-        <CardActions
-          style={{ display: "flex", alignItems: "center", height: 45 }}
-        >
-          <Tooltip title="Add to cart">
-            <IconButton
-              size="small"
-              style={{ marginTop: 40, marginLeft: 150 }}
-              onClick={e => {
-                e.stopPropagation();
-                this.props.dispatch(
-                  addItemInCart({ ...this.props.item, quantity: 1 })
-                );
-              }}
-              color="primary"
-              aria-label="Add to shopping cart"
-            >
-              <AddShoppingCartIcon size="small" />
-            </IconButton>
-          </Tooltip>
-        </CardActions>
-      </Card>
-      */
     );
   }
 }
