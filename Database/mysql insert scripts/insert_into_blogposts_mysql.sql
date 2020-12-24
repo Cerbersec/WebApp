3418f9ce9083b1cd417a390d58afdbc8cd397738
Cerbersec <58052983+Cerbersec@users.noreply.github.com>
2020-12-24 03:56:32 +0100

    restructured database scripts, generated new insert scripts for categories and products, added accessories

---
+++ Database/mysql insert scripts/insert_into_blogposts_mysql.sql
@@ -0,0 +1,39 @@
+INSERT INTO blogposts(title, post_date, content,created_at, updated_at)
+VALUES ('Brantayes, The next big e-commerce shop', '2020-12-09', 'Brantayes is taking over the world of online shoes! 
+Once upon a time in a small city in Belgium, we were just snipping of our bear and dreaming about a lot of things. Until one of u said Imagine we could take over the market of shoes and just develop a total e-commerce platform. And yes, that was the moment that we started our journey. 
+
+In a couple of months, we  just started from scratch and created a whole new web shop with over 1000 shoes and even relevant products such as socks, bels and soles. And of course, we are adding more products every day! 
+At Brantayes, we will offer everyone, from 1 year until 101 year shoes, a shoe that they like. 
+
+You can just sing up on our website, receive our monthly newsletters with new promos, shoes and more, and together we will build up a beautiful online shoe shop!
+
+Hit us up if you have any questions about it! We look forward to you see you as a customer at our web shop!
+
+The Brantayes-team!
+','2020-12-09','2020-12-09')
+
+INSERT INTO blogposts(title, post_date, content,created_at, updated_at)
+VALUES ('6 reasons why you should wear sneakers!', '2020-12-09','Sneakers are trending, sneakers hip, sneakers are cool, sneakers are just everywhere. Are you still not convinced of wearing sneakers? Just read this short article and we ensure you, after reading this, you will immediately buy sneakers at Brantayes!
+
+1.	Comfortable 
+If you think about sneakers, you probably think at the comfort they give you if you wear them. Because of their soft soles and a good support for your heels, it feels like you are running in the sky. Tie your shoes good and you will be able to walk comfortable all day. So throw away the high heels and wear sneakers!
+
+2.	Perfect for every whether
+If it is raining or the sun is shining or there is even snow, sneakers are perfect for every whether circumstance. Wear some nice white sneakers in summer and black or dark green in winter. You will shine anytime!
+
+3.	Perfect for at a festival, at the bureau of for shopping
+Initially, sneakers were developed for sporters, because they are very comfortable to wear and they offer a good support for your feeds. But you are still able to wear them anytime, even if you are not doing a sport. These days, sneakers are perfect to wear if you go to your job, when you go to a festival (because jumping on high heels doesn’t seem that comfortable) or even when you go shopping. 
+
+4.	From bad-ass to classic
+We have sneakers in all sizes, for all ages and all sexes. A sneaker could maybe have the reputation of a sportive shoe, but this is not true. At Brantayes, you will find a lot of sneakers that are stylish.
+
+5.	Combination possibilities are endless
+Of course is wearing a sneaker with a skinny jeans a very good combination. But you can still combine them with 100 other possibilities. A nice pant, a short skirt or a stylish dress, it are just a few possibilities.
+
+6.	Perfect for every age
+Sneakers are for everyone, whether you are 5 years old or 80 years old, you can wear them. We believe that everyone could be a fan of sneakers. Toddlers or children could wear sneakers with hook and loop fasteners, teenagers wear them with some eye-catching patterns, parents elegant leather sneakers and grandparents stylish sneakers.
+
+Did we enthuse you about sneakers? 
+Do not hesitate to visit our online shop at www.brantayes.be 
+
+The Brantayes-team!','2020-12-09','2020-12-09')
---
+++ Database/mysql insert scripts/insert_into_categories_mysql.sql
@@ -0,0 +1,9 @@
+INSERT INTO categories(category_name, product_group, created_at, updated_at) VALUES
+	('Men', 'Shoes', '2020-10-15', '2020-10-15'),
+	('Women', 'Shoes', '2020-10-15', '2020-10-15'),
+	('Boys', 'Shoes', '2020-10-15', '2020-10-15'),
+	('Girls', 'Shoes', '2020-10-15', '2020-10-15'),
+	('Other', 'Shoes', '2020-10-15', '2020-10-15'),
+	('Men', 'Accessories', '2020-10-15', '2020-10-15'),
+	('Women', 'Accessories', '2020-10-15', '2020-10-15'),
+	('Other', 'Accessories', '2020-10-15', '2020-10-15');
