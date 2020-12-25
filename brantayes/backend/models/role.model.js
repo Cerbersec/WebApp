c2e6a17fa1469aeb02c6d72c5df39837ba875dc9
Cerbersec <58052983+Cerbersec@users.noreply.github.com>
2020-12-25 20:57:23 +0100

    created model for role

---
+++ brantayes/backend/models/role.model.js
@@ -0,0 +1,26 @@
+module.exports = function(sequelize, Sequelize) {
+    const Role = sequelize.define('Role', {
+        role_id: {
+            autoIncrement: true,
+            primaryKey: true,
+            type: Sequelize.INTEGER
+        },
+        name: {
+            type: Sequelize.STRING(50),
+            allowNull: false
+        },
+    },
+    {
+        underscored: true
+    })
+
+    Role.associate = models => {
+        Role.belongsToMany(models.Administrator, {
+            through: "user_roles",
+            foreignKey: "role_id",
+            otherKey: "user_id",
+        })
+    }
+
+    return Role
+}
