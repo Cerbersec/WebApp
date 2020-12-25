b00befb20767e84e830d37c2bdd9beb32e2bdc28
Cerbersec <58052983+Cerbersec@users.noreply.github.com>
2020-12-26 00:09:25 +0100

    added authController

--- brantayes/backend/controllers/index.js
+++ brantayes/backend/controllers/index.js
@@ -1,7 +1,9 @@
 const storeController = require('./store.controller')
 const userController = require('./user.controller')
 const blogController = require('./blog.controller')
+const authController = require('./auth.controller')
 
 exports.storeController = storeController
 exports.userController = userController
 exports.blogController = blogController
+exports.authController = authController
