97d0d069581e8db2925f8bf41954d21b60d6df4c
Cerbersec <58052983+Cerbersec@users.noreply.github.com>
2020-12-26 01:36:18 +0100

    modified route endpoints and replaced middleware with new middleware authJwt and verifySignup

--- brantayes/backend/routes/user.router.js
+++ brantayes/backend/routes/user.router.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const { userController } = require('../controllers')
-const { authJwt } = require('../middleware/authJwt')
+const { authJwt } = require('../middleware')
 
 router.get('/details', authJwt.verifyToken, userController.getUserByID)
 
