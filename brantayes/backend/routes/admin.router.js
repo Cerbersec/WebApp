c7d52395677e093853faa3f25e8fcf3e3057417a
Kirsten V <72037380+keverken@users.noreply.github.com>
2021-01-07 00:43:25 +0100

    small router fixes

--- brantayes/backend/routes/admin.router.js
+++ brantayes/backend/routes/admin.router.js
@@ -5,7 +5,7 @@ const { authJwt } = require('../middleware')
 
 router.get('/', [authJwt.verifyToken, authJwt.isAdministrator], adminController.test)
 router.post('/products/add', [authJwt.verifyToken, authJwt.isAdministrator], adminController.putProduct)
-router.post('/products/update', [authJwt.verifyToken, authJwt.isAdministrator], adminController.updateProduct)
-router.get('/products/delete', [authJwt.verifyToken, authJwt.isAdministrator], adminController.removeProduct)
+router.post('/products/update/:product_id', [authJwt.verifyToken, authJwt.isAdministrator], adminController.updateProduct)
+router.get('/products/delete/:product_id', [authJwt.verifyToken, authJwt.isAdministrator], adminController.removeProduct)
 
 module.exports = router
--- brantayes/backend/routes/info.router.js
+++ brantayes/backend/routes/info.router.js
@@ -4,7 +4,7 @@ const { infoController } = require('../controllers')
 const { authJwt } = require('../middleware')
 
 router.get('/shipping', infoController.GetShippingCosts)
-router.get('/UpdateShipping', infoController.PostShippingCosts)
+router.post('/UpdateShipping', [authJwt.verifyToken, authJwt.isAdministrator], infoController.PostShippingCosts)
 router.post('/updatelogo', [authJwt.verifyToken, authJwt.isAdministrator], infoController.updateLogo)
 
 module.exports = router
