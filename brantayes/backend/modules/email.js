c02f99ab53e36dcd60f9945e5a9d0b665bfc88a1
yungxog <62614490+yungxog@users.noreply.github.com>
2021-01-16 17:10:04 +0100

    Fix 2

--- brantayes/backend/modules/email.js
+++ brantayes/backend/modules/email.js
@@ -32,15 +32,15 @@ const resetPasswordTemplate = (user, url) => {
   return { from, to, subject, html }
 }
 
-const orderConfirmationTemp = (user) => {
+const orderConfirmationTemp = (email) => {
   const from = process.env.EMAIL_LOGIN
-  const to = user.email_address
+  const to = email
   const subject = "Your order has been shipped!"
   const html = `
-  <p>Hey ${user.username || user.email},</p>
+  <p>Hey ${email}!</p>
   <p>We heard that you lost your Brantayes password. Sorry about that!</p>
   <p>But don’t worry! You can use the following link to reset your password:</p>
-  <a href=${url}>${url}</a>
+  
   <p>If you don’t use this link within 1 hour, it will expire.</p>
   <p>–The Brantayes team</p>
   `
